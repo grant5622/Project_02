07d1ac81554d7f936a1dcce35eb15f1dacc4005c
ansonshendrick <113936107+ansonshendrick@users.noreply.github.com>
2023-06-05 18:44:31 -0500

    push

--- models/User.js
+++ models/User.js
@@ -1,26 +1,54 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
-    user_id: {
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primarykey: true,
+      primaryKey: true,
       autoIncrement: true,
     },
-
-    user_name: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
   },
   {
-    sequalize,
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "User",
+    modelName: 'user',
   }
 );
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,28 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-async function test(){
-    require('dotenv').config();
-    const axios = require('axios');
-    const API_KEY=process.env.API_KEY;
-    const options = {
-      method: 'GET',
-      url: 'https://streaming-availability.p.rapidapi.com/v2/get/basic',
-      params: {
-        country: 'us',
-        imdb_id: 'tt1877830',
-        output_language: 'en'
-      },
-      headers: {
-        'X-RapidAPI-Key': API_KEY,
-        'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-      }
-    };
-    
-    try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        console.log(response.data.result.streamingInfo.us);
-    } catch (error) {
-        console.error(error);
-    }
-}
-test();
+const sequelize = require('./config/connection');
+
+// Create a new sequelize store using the express-session package
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+// Configure and link a session object with the sequelize store
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Add express-session and store as Express.js middleware
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
